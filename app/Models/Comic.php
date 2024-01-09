75ce08fe142a00d07adeda72c59398a371283061
Eecodev <mnasser.bbk@gmail.com>
2024-01-09 17:43:32 +0100

    variable $fillable added to model to save data on db

--- app/Models/Comic.php
+++ app/Models/Comic.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Comic extends Model
 {
     use HasFactory;
+    // metto qua quelli che non voglio salvare (quelli che voglio escludere)
+    // protected $guarded = [];
+
+    // or
+    
+    // i dati che voglio salvare sul database
+    protected $fillable = ['title', 'description', 'price', 'type'];
 }
